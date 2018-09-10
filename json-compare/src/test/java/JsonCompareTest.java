fa34c0cc2323cb6123ee422e9733e1f40bf36df8
Kaushik Bagchi <kaushik.bagchi@ing.com>
2018-09-10 16:38:54 +0200

    changes in the json file path for travis issue

--- json-compare/src/test/java/JsonCompareTest.java
+++ json-compare/src/test/java/JsonCompareTest.java
@@ -16,24 +16,24 @@ public class JsonCompareTest {
 	@Test
 	public void testJsonCompare() throws MalformedURLException, IOException, URISyntaxException, InstantiationException, IllegalAccessException {
 		Assert.assertTrue(JsonCompare.compare(
-				new File("src\\test\\resources\\superSet2.json"), 
-				new File("src\\test\\resources\\subSet2.json"),
+				new File("src/test/resources/superSet2.json"), 
+				new File("src/test/resources/subSet2.json"),
 				null).size()>0);
 	}
 	
 	@Test
 	public void testJsonCompareValue() throws MalformedURLException, IOException, URISyntaxException, InstantiationException, IllegalAccessException {
 		Assert.assertTrue(JsonCompare.compare(
-				new File("src\\test\\resources\\superSet3.json"), 
-				new File("src\\test\\resources\\subSet3.json"),
+				new File("src/test/resources/superSet3.json"), 
+				new File("src/test/resources/subSet3.json"),
 				new ValueRule()).size()==0);
 	}
 	
 	@Test
 	public void testJsonCompareValueType() throws MalformedURLException, IOException, URISyntaxException, InstantiationException, IllegalAccessException {
 		Assert.assertTrue(JsonCompare.compare(
-				new File("src\\test\\resources\\superSetType.json"), 
-				new File("src\\test\\resources\\subSetType.json"),
+				new File("src/test/resources/superSetType.json"), 
+				new File("src/test/resources/subSetType.json"),
 				new TypeRule()).size()==0);
 	}
 	
